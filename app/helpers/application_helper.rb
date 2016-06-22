a45b54c91bede54a3b3613e56524629854acbf39
Samdulsey <samuelwanjau8172@gmail.com>
2016-06-22 12:41:20 +0300

    morning idea

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+#Retuns the full title on a per-page basis
+def full_title(page_title = '')
+	base_title = "arana"
+	if page_title.empty?
+		base_title
+	else
+		"#{page_title} | #{base_title}"
+	end
+end
+	
+end
+
 end
