35b4008cfae9a8253b7a9483868343e32d9a806a
Samdulsey <samuelwanjau8172@gmail.com>
2016-06-29 15:08:27 +0300

    Modify outlook and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root           'static_pages#home'
 
-  get 'static_pages/help'
+  get  'help'  => 'static_pages#help'
 
-  get 'static_pages/about'
+  get  'about'  => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get  'contact'  => 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
